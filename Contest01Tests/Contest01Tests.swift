7b985f554d0f11aae4e9704d271682fa4bdd7861
Angie Yan <angieyan@Andys-MacBook-Air.local>
2019-11-05 20:18:51 -0500

    We created a new empty xcode project for contest01.

---
+++ Contest01Tests/Contest01Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Contest01Tests.swift
+//  Contest01Tests
+//
+//  Created by Angie Yan on 2019-11-05.
+//  Copyright © 2019 Angie Yan. All rights reserved.
+//
+
+import XCTest
+@testable import Contest01
+
+class Contest01Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
