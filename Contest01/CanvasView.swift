460466a1e908be3ac5db5adee96664d85bd26b18
Lambert Lin <lambertlin@mayingdeMacBook-Pro.local>
2019-11-06 22:55:50 +0800

    We added a canvas.

---
+++ Contest01/CanvasView.swift
@@ -0,0 +1,22 @@
+//
+//  CanvasView.swift
+//  Contest01
+//
+//  Created by Lambert Lin on 2019/11/6.
+//  Copyright © 2019 Angie Yan. All rights reserved.
+//
+
+import UIKit
+
+class CanvasView: UIView {
+
+    
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+        
+    }
+   
+
+}
