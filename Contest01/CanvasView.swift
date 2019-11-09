ce245f9ad13c9a985e1b4b61d866815f64fbf598
Hal Wang <halhaoranwang@gmail.com>
2019-11-09 20:57:05 +0800

    each line has a different colour

--- Contest01/CanvasView.swift
+++ Contest01/CanvasView.swift
@@ -11,9 +11,33 @@ import UIKit
 class CanvasView: UIView {
 
     override func draw(_ rect: CGRect) {
-        let pencil = UIBezierPath()
-        pencil.move(to: CGPoint(x: 100, y: 300))
-        pencil.addLine(to: CGPoint(x: 300, y: 300))
-        pencil.stroke()
+        drawLines()
+        
+        
+    }
+    
+    func drawLines() {
+        let width: Int = Int(bounds.width)
+        let height: Int = Int(bounds.height)
+        
+        for _ in 0 ..< 100 {
+            let pencil = UIBezierPath()
+            let randomStartX: CGFloat = CGFloat(Int(arc4random()) % width)
+            let randomStartY: CGFloat = CGFloat(Int(arc4random()) % height)
+            let randomEndX: CGFloat = CGFloat(Int(arc4random()) % width)
+            let randomEndY: CGFloat = CGFloat(Int(arc4random()) % height)
+            
+            pencil.move(to: CGPoint(x: randomStartX, y: randomStartY))
+            pencil.addLine(to: CGPoint(x: randomEndX, y: randomEndY))
+            
+            let red = CGFloat(arc4random() % 1000) / 1000
+            let green = CGFloat(arc4random() % 1000) / 1000
+            let blue = CGFloat(arc4random() % 1000) / 1000
+            
+            let colour = UIColor(red: red, green: green, blue: blue, alpha: 1)
+            colour.setStroke()
+            pencil.lineWidth = 4
+            pencil.stroke()
+        }
     }
 }
