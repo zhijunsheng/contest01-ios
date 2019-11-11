182bfb7a8e034cca00ee5e2b9ffe25423f73c4d8
Angie Yan <angieyan@Andys-MacBook-Air.local>
2019-11-11 17:38:58 -0500

    I drew a simple emoji using shapes!

--- Contest01/CanvasView.swift
+++ Contest01/CanvasView.swift
@@ -10,21 +10,31 @@ import UIKit
 
 class CanvasView: UIView {
     override func draw(_ rect: CGRect) {
-        randomShapes()
         randomCircles()
+        drawEmoji()
     }
     
-    func randomShapes() {
-        for _ in 0..<100 {
-            let randomX = CGFloat(arc4random() % 400)
-            let randomY = CGFloat(arc4random() % 400)
-            let square = UIBezierPath(rect: CGRect(x: randomX, y: randomY, width: 80, height: 80))
-            square.stroke()
-        }
+    
+    func drawEmoji() {
+        let face = UIBezierPath(arcCenter: CGPoint(x: 350, y: 400), radius: 350, startAngle: 0, endAngle: CGFloat.pi * 2, clockwise: true)
+        #colorLiteral(red: 0.9577738612, green: 0.9686274529, blue: 0.0223935303, alpha: 1).setFill()
+        face.fill()
+        
+        let circle13 = UIBezierPath(arcCenter: CGPoint(x: 150, y: 350), radius: 50, startAngle: 0, endAngle: CGFloat.pi * 2, clockwise: true)
+        #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1).setFill()
+        circle13.fill()
+        
+        let circle17 = UIBezierPath(arcCenter: CGPoint(x: 550, y: 350), radius: 50, startAngle: 0, endAngle: CGFloat.pi * 2, clockwise: true)
+        #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1).setFill()
+        circle17.fill()
+        
+        let smile = UIBezierPath(arcCenter: CGPoint(x: 350, y: 500), radius: 150, startAngle: 0, endAngle: CGFloat.pi, clockwise: true)
+        #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1).setFill()
+        smile.fill()
     }
     
     func randomCircles() {
-        for _ in 0..<50 {
+        for _ in 0..<60 {
             let randomX = CGFloat(arc4random() % 700)
             let randomY = CGFloat(arc4random() % 700)
             let randomRadius = CGFloat(arc4random() % 400)
